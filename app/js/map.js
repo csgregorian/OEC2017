7feed4711883f37be5133584ceb4916b1a8b5bf7
Christopher Gregorian <csgregorian@gmail.com>
2017-01-27 21:32:54 -0500

    more nodes

--- app/js/map.js
+++ app/js/map.js
@@ -1,86 +1,290 @@
 var elements = [
   {
-      "data": { "id": 34 }
+    "data": {
+      "id": 34,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 33 }
+    "data": {
+      "id": 33,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 32 }
+    "data": {
+      "id": 32,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 31 }
+    "data": {
+      "id": 31,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 23 }
+    "data": {
+      "id": 23,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 22 }
+    "data": {
+      "id": 22,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 21 }
+    "data": {
+      "id": 21,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 41 }
+    "data": {
+      "id": 41,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 42 }
+    "data": {
+      "id": 42,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 43 }
+    "data": {
+      "id": 43,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 13 }
+    "data": {
+      "id": 13,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 12 }
+    "data": {
+      "id": 12,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 11 }
+    "data": {
+      "id": 11,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 51 }
+    "data": {
+      "id": 51,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 52 }
+    "data": {
+      "id": 52,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 53 }
+    "data": {
+      "id": 53,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 61 }
+    "data": {
+      "id": 61,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 62 }
+    "data": {
+      "id": 62,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 63 }
+    "data": {
+      "id": 63,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 64 }
+    "data": {
+      "id": 64,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 65 }
+    "data": {
+      "id": 65,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 71 }
+    "data": {
+      "id": 71,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 72 }
+    "data": {
+      "id": 72,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 73 }
+    "data": {
+      "id": 73,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 81 }
+    "data": {
+      "id": 81,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 82 }
+    "data": {
+      "id": 82,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 91 }
+    "data": {
+      "id": 91,
+      "type": "house"
+    }
   },
   {
-      "data": { "id": 92 }
-  }
+    "data": {
+      "id": 92,
+      "type": "house"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-01",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-02",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-03",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-04",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-05",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-06",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-07",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-08",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-09",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1-10",
+      "type": "control"
+    }
+  },
+    {
+      "data": {
+        "id": "A-2-01",
+        "type": "control"
+      }
+    },
+  {
+    "data": {
+      "id": "A-2-02",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-2-03",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-2-04",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-2-05",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-2-06",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-2-07",
+      "type": "control"
+    }
+  },
+  {
+    "data": {
+      "id": "A-1",
+      "type": "generator"
+    }
+  },
+  {
+    "data": {
+      "id": "A-2",
+      "type": "generator"
+    }
+  },
+  {
+    "data": {
+      "id": "B-1",
+      "type": "generator"
+    }
+  },
 ]
